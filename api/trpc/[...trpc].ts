c67cb64b0ed9307de38057ccfb4a60f4256c2491
Puranjay Yadav <puranjayrocks4@gmail.com>
2026-01-19 12:44:02 -0500

    fix: ignore ts error for bundled api

--- api/trpc/[...trpc].ts
+++ api/trpc/[...trpc].ts
@@ -1,5 +1,6 @@
 // This file uses the pre-bundled API code from api/dist/index.js
 // The bundling is done by npm run build:api which includes all dependencies
+// @ts-ignore
 import handler from "../dist/index.js";
 
 export default handler;
